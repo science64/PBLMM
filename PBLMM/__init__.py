eea9dadcd9efc1d407f3ac7cfd7cb4311788a832
Süleyman Bozkurt <sbozkurt.mbg@gmail.com>
2023-10-22 01:47:22 +0200

    version 2.0

--- PBLMM/__init__.py
+++ PBLMM/__init__.py
@@ -1,6 +1,9 @@
-__author__ = 'Kevin Klann'
-__updated_by__ = 'Süleyman Bozkurt'
-__version__ = '2.0'
-__updated_date__ = '2023-10-22'
-__description__ = 'PBLMM - updates and fixes.'
-__license__ = 'MIT'
+# __author__ = 'Kevin Klann'
+# __updated_by__ = 'Süleyman Bozkurt'
+# __version__ = '2.0'
+# __updated_date__ = '2023-10-22'
+# __description__ = 'PBLMM - updates and fixes.'
+# __license__ = 'MIT'
+# __init__.py
+
+from .PBLMM import *
