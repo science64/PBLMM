bf03114e929fff56a463aab4a55b13be65b0b7db
Süleyman Bozkurt <sbozkurt.mbg@gmail.com>
2026-02-02 13:20:23 +0100

    v2.1.2 - Code did not changed just metadata information is activated. Uncomment metadata in __init__.py for clarity and accessibility

--- PBLMM/__init__.py
+++ PBLMM/__init__.py
@@ -1,9 +1,8 @@
-# __author__ = 'Kevin Klann'
-# __updated_by__ = 'Süleyman Bozkurt'
-# __version__ = '2.1.2'
-# __updated_date__ = '2025-11-25'
-# __description__ = 'PBLMM - updates and fixes.'
-# __license__ = 'MIT'
-# __init__.py
+__author__ = 'Kevin Klann'
+__updated_by__ = 'Süleyman Bozkurt'
+__version__ = '2.1.2'
+__updated_date__ = '2025-11-25'
+__description__ = 'PBLMM - updates and fixes.'
+__license__ = 'MIT'
 
 from .PBLMM import *
